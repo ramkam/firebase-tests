4f64901019f42edb3ece7a745f61b5c3caba4cb9
ramkam <contact@ramkam.com>
2014-02-04 08:17:47 +0100

    example 2

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
-angular.module("myChatRoom", ["firebase"])
-  .factory("ChatService", ["$firebase", function($firebase) {
-    var ref = new Firebase("https://blistering-fire-302.firebaseio.com/chat");
+
+angular.module("sampleApp", ["firebase"])
+  .factory("sampleService", ["$firebase", function($firebase) {
+    var ref = new Firebase("https://blistering-fire-302.firebaseio.com/text");
     return $firebase(ref);
   }])
-  .controller("ChatController", ["$scope", "ChatService",
-    function($scope, chatService) {
-      $scope.user = "Guest " + Math.round(Math.random()*101);
-      $scope.messages = chatService;
-      $scope.addMessage = function() {
-        $scope.messages.$add({from: $scope.user, content: $scope.message});
-        $scope.message = "";
-      };
+  .controller("SampleController", ["$scope", "sampleService",
+    function($scope, service) {
+      service.$bind($scope, "text");
     }
   ]);
--- index.php
+++ index.php
@@ -1,17 +1,12 @@
-<html ng-app="myChatRoom">
+<html ng-app="sampleApp">
   <head>
     <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.7/angular.min.js"></script>
     <script src="//cdn.firebase.com/js/client/1.0.2/firebase.js"></script>
     <script src="//cdn.firebase.com/libs/angularfire/0.6.0/angularfire.min.js"></script>
     <script src="app.js"></script>
   </head>
-  <body ng-controller="ChatController">
-    <ul ng-repeat="message in messages">
-      <li>{{message.from}}: {{message.content}}</li>
-    </ul>
-    <form ng-submit="addMessage();">
-      <input type="text" ng-model="message"/>
-      <input type="submit" value="Send Message"/>
-    </form>
+  <body ng-controller="SampleController">
+    <input type="text" ng-model="text"/>
+    <h1>You said: {{text}}</h1>
   </body>
 </html>
